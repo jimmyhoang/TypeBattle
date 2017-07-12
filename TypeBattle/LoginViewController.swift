8e9f680e9cbddf2a0b9ee74ab29a165bed607d65
Jimmy Hoang <hoangjimmy94@gmail.com>
2017-07-12 16:31:15 -0700

    Implement Facebook login

---
+++ TypeBattle/LoginViewController.swift
@@ -0,0 +1,45 @@
+//
+//  LoginViewController.swift
+//  TypeBattle
+//
+//  Created by Jimmy Hoang on 2017-07-12.
+//  Copyright © 2017 Jimmy Hoang. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+import FacebookLogin
+
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let loginButton = LoginButton(readPermissions: [.publicProfile])
+        loginButton.center = view.center
+        
+        view.addSubview(loginButton)
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+        
+
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
