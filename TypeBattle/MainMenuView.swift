07f8c5150e2f88bc54c0e6a0a5e935e8079b1221
Alex Lee <x.lee@mail.utoronto.ca>
2017-07-13 13:24:16 -0700

    Initial Main Menu VC setup, need to fix custom font being clipped on any UILabel elements (subclass/extend and resize?)

---
+++ TypeBattle/MainMenuView.swift
@@ -0,0 +1,101 @@
+//
+//  MainMenuView.swift
+//  TypeBattle
+//
+//  Created by Alex Lee on 2017-07-13.
+//  Copyright © 2017 Jimmy Hoang. All rights reserved.
+//
+
+import UIKit
+
+class MainMenuView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+    private lazy var nameLabel:UILabel = {
+        let label = UILabel()
+        label.textColor = UIColor.black
+        label.contentMode = .scaleAspectFit
+        label.font = UIFont.gameFont(size: 50.0)
+        label.text = "Type Battle"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var stack:UIStackView = {
+        let sv = UIStackView()
+        sv.alignment = UIStackViewAlignment.fill
+        sv.translatesAutoresizingMaskIntoConstraints = false
+        sv.isLayoutMarginsRelativeArrangement = true
+        sv.layoutMargins = UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
+        sv.axis = .vertical
+        sv.spacing = 8
+        return sv
+    }()
+    
+    private lazy var singlePlayerButton:UIButton = {
+        let button = UIButton()
+        button.contentVerticalAlignment = .fill
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = UIColor(colorLiteralRed: 196.0/255.0, green: 48.0/255.0, blue: 43.0/255.0, alpha: 1.0)
+        button.setTitle("Single player", for: .normal)
+        button.setTitleColor(UIColor.white, for: .normal)
+        button.titleLabel?.font = UIFont.gameFont(size: 30.0)
+        return button
+    }()
+    
+    private lazy var multiPlayerButton:UIButton = {
+        let button = UIButton()
+        button.contentVerticalAlignment = .fill
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = UIColor(colorLiteralRed: 196.0/255.0, green: 48.0/255.0, blue: 43.0/255.0, alpha: 1.0)
+        button.setTitle("Multi-player", for: .normal)
+        button.setTitleColor(UIColor.white, for: .normal)
+        button.titleLabel?.font = UIFont.gameFont(size: 30.0)
+        return button
+    }()
+    
+    private lazy var settingsButton:UIButton = {
+        let button = UIButton()
+        button.contentVerticalAlignment = .fill
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = UIColor(colorLiteralRed: 196.0/255.0, green: 48.0/255.0, blue: 43.0/255.0, alpha: 1.0)
+        button.setTitle("Settings", for: .normal)
+        button.setTitleColor(UIColor.white, for: .normal)
+        button.titleLabel?.font = UIFont.gameFont(size: 30.0)
+        return button
+    }()
+    
+//    override init(frame: CGRect) {
+//        super.init(frame: frame)
+//        self.addSubview(nameLabel)
+//        self.setNeedsUpdateConstraints()
+//    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.addSubview(nameLabel)
+        self.addSubview(stack)
+        
+        stack.addArrangedSubview(singlePlayerButton)
+        stack.addArrangedSubview(multiPlayerButton)
+        stack.addArrangedSubview(settingsButton)
+        
+        self.setNeedsUpdateConstraints()
+    }
+    
+    override func updateConstraints() {
+        NSLayoutConstraint.activate([ nameLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+                                      nameLabel.topAnchor.constraint(equalTo: topAnchor, constant: 50.0),
+                                      stack.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 25.0),
+                                      stack.centerXAnchor.constraint(equalTo: centerXAnchor),
+                                      stack.widthAnchor.constraint(equalTo: nameLabel.widthAnchor)])
+        super.updateConstraints()
+    }
+}
