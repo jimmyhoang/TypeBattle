f1ba55a69273bcde907ed1456f570ebee70f5b7a
Jimmy Hoang <hoangjimmy94@gmail.com>
2017-07-13 14:52:05 -0700

    Customize login screen, add FireBase login/authentication, fixed Facebook login support

--- TypeBattle/AppDelegate.swift
+++ TypeBattle/AppDelegate.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import Firebase
+import FacebookLogin
+import FacebookCore
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -39,11 +41,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        AppEventsLogger.activate(application)
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
+    
+    func application(_ application:UIApplication,open openURLurl:URL,sourceApplication:String?,annotation:Any)->Bool {
+        return SDKApplicationDelegate.shared.application(application, didFinishLaunchingWithOptions:[:])
+    }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+        return SDKApplicationDelegate.shared.application(app, open: url, options: options)
+    }
+
 
 }
 
