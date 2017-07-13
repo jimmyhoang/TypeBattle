eba9d24ffb8fa48876d2b4b174aefe78f23813d5
Alex Lee <x.lee@mail.utoronto.ca>
2017-07-13 11:53:59 -0700

    Add game font as extension to UIFont

---
+++ TypeBattle/UIFont+CustomFonts.swift
@@ -0,0 +1,16 @@
+//
+//  UIFont+CustomFonts.swift
+//  TypeBattle
+//
+//  Created by Alex Lee on 2017-07-13.
+//  Copyright © 2017 Jimmy Hoang. All rights reserved.
+//
+
+import UIKit
+
+extension UIFont {
+    static func gameFont(size:CGFloat) -> UIFont {
+        
+        return UIFont(name: "Supersonic Rocketship", size: size)!
+    }
+}
