0a41cf0758e8a68982a0d85bf141334b80888fbb
hli30 <harrylll93@gmail.com>
2017-07-14 04:54:49 -0700

    add background sky, ground, addkeystrokeDetection

---
+++ TypeBattle/KeystrokeDetection.swift
@@ -0,0 +1,28 @@
+//
+//  KeystrokeDetection.swift
+//  TypeBattle
+//
+//  Created by Harry Li on 2017-07-14.
+//  Copyright © 2017 Jimmy Hoang. All rights reserved.
+//
+
+import UIKit
+
+class KeystrokeDetection:NSObject, UITextFieldDelegate {
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        switch string {
+        case "a":
+            <#code#>
+        case "b":
+            <#code#>
+        case "c":
+            <#code#>
+        case "d":
+            <#code#>
+        default:
+            <#code#>
+        }
+    }
+    
+}
