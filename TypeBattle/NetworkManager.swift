3a7c81f4f67eaed7b46199b7d9bdd88166441bdf
Jimmy Hoang <hoangjimmy94@gmail.com>
2017-07-19 12:36:56 -0700

    Formatting

--- TypeBattle/NetworkManager.swift
+++ TypeBattle/NetworkManager.swift
@@ -27,7 +27,7 @@ class NetworkManager{
                         let newPlayer = Player(name: nickname, playerID: user.uid,avatarName: avatarName)
                         let playerRef = ref.child(user.uid.lowercased())
                         
-                        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+                        let appDelegate    = UIApplication.shared.delegate as! AppDelegate
                         appDelegate.player = newPlayer
                         
                         playerRef.setValue(newPlayer.toAnyObject())
@@ -39,7 +39,7 @@ class NetworkManager{
     }
     
     class func login(email: String, password: String, completion:@escaping (Bool?, String?) -> (Void)){
-        var loginSuccess = false
+        var loginSuccess     = false
         var errorDescription = ""
         
         Auth.auth().signIn(withEmail: email, password: password) { (user, error) in
@@ -48,7 +48,7 @@ class NetworkManager{
                 loginSuccess = true
             }
             else {
-                guard let error = error else {return}
+                guard let error  = error else {return}
                 errorDescription = error.localizedDescription
             }
             completion(loginSuccess, errorDescription)
@@ -68,7 +68,7 @@ class NetworkManager{
             case .cancelled:
                 errorDescription = "Facebook connect cancelled!"
             case .success(_,_,_):
-                let credential = FacebookAuthProvider.credential(withAccessToken: (AccessToken.current?.authenticationToken)!)
+                let credential   = FacebookAuthProvider.credential(withAccessToken: (AccessToken.current?.authenticationToken)!)
                 
                 Auth.auth().signIn(with: credential, completion: { (user, error) in
                     if error == nil {
@@ -85,13 +85,11 @@ class NetworkManager{
                                 let photoDict                = responseDictionary["picture"] as! NSDictionary
                                 let dataDict                 = photoDict["data"] as! NSDictionary
                                 
-                                let name       = responseDictionary["name"] as! String
-                                guard let url  = dataDict["url"] else {return}
-                                
+                                let name            = responseDictionary["name"] as! String
+                                guard let url       = dataDict["url"] else {return}
                                 guard let stringURL = URL(string: url as! String) else {return}
-                                
-                                guard let user = user else {return}
-                                let newPlayer  = Player(name: name, playerID: user.uid, avatarName: "")
+                                guard let user      = user else {return}
+                                let newPlayer       = Player(name: name, playerID: user.uid, avatarName: "")
                                 
                                 DispatchQueue.main.async {
                                     downloadFBImage(url: stringURL, completion: { (image) -> (Void) in
@@ -140,7 +138,7 @@ class NetworkManager{
             newPlayer.levelProgression = levelProgression
             newPlayer.matchesPlayed    = matchesPlayed
             newPlayer.matchesWon       = matchesWon
-            appDelegate.player        = newPlayer
+            appDelegate.player         = newPlayer
         })
 
     }
