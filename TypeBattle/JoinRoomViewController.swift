eaee7edd4c6cd441d5fbb71e2f0f31862ffc4214
Fernando Jinzenji <fernandoj@me.com>
2017-07-19 14:32:49 -0700

    removed player if home button is pressed

--- TypeBattle/JoinRoomViewController.swift
+++ TypeBattle/JoinRoomViewController.swift
@@ -114,6 +114,25 @@ class JoinRoomViewController: UIViewController, UITableViewDelegate, UITableView
             }
             self.currentGameSession = gameSession
             
+            // user not in players collection (for example, pressed the home button), sent to mainscreen
+            var isInGame = false
+            for p in self.currentGameSession.players {
+                if (p.playerID == self.currentPlayer.playerID) {
+                    isInGame = true
+                    break
+                }
+            }
+            if (!isInGame) {
+                DispatchQueue.main.async(execute: {
+                    let alert = UIAlertController(title: "Player not in room", message: "You are not in this room anymore. Please join again or choose another room", preferredStyle: UIAlertControllerStyle.alert)
+                    alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (action) in
+                        self.GoToMainLobby()
+                    }))
+                    self.present(alert, animated: true, completion: nil)
+                })
+                return
+            }
+            
             // Reload table view
             self.tableView.reloadData()
             
